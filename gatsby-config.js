e48bb00f6ad132eb2efea3e21842c696c5364f67
russreeves5 <russreeves5@gmail.com>
2022-10-02 08:30:59 -0400

    Update Site Info
    Updated url, site description, etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Ventrum`,
+    siteTitleAlt: `Ventrum Health`,
+    siteHeadline: `Ventrum Health - Innovating Minimally Invasive Medical Treatments`,
+    siteUrl: `https://ventrum.health`,
+    siteDescription: `Company Description`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Russ@Ventrum.Health`,
   },
   plugins: [
     {
